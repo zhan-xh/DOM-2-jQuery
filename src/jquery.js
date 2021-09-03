c82c0ac0f5280109e847b6753199cb6efcab269d
xiaohongZHAN <2459522339@qq.com>
2021-09-03 14:35:18 +0800

    闭包&链式操作return this

--- src/jquery.js
+++ src/jquery.js
@@ -1,3 +1,12 @@
-window.jQuery = function () {
-    console.log('我是jQuery')
+window.jQuery = function (selector) {
+    const elements = document.querySelectorAll(selector)
+    return {
+        addClass(className) {
+            for (let i = 0; i < elements.length; i++) {
+                elements[i].classList.add(className)
+            }
+            return this//保证了链式操作 this就是api  当obj.fn()时,this就是obj
+        }
+    }
+    // return api //返回一个可以操作elements的对象
 }
--- src/main.js
+++ src/main.js
@@ -1,1 +1,5 @@
-jQuery()
+const api = jQuery(".test")//不反悔元素们，返回api对象
+api.addClass('red')
+    .addClass('blue')
+    .addClass('green')//因为api.addClass('red')返回的是api(一个对象),所以就还可以对api.addClass('red')进行点操作
+//这就是链式操作
